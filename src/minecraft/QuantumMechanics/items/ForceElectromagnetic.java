287466256eaf3633c28b2b7a4eea0918ecd5c232
Riaku <riaku56@gmail.com>
2013-10-20 16:45:52 -0500

    Adding some stuff to play with later

---
+++ src/minecraft/QuantumMechanics/items/ForceElectromagnetic.java
@@ -0,0 +1,35 @@
+package QuantumMechanics.items;
+
+import java.util.List;
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class ForceElectromagnetic extends Item {
+
+	@SideOnly(Side.CLIENT)
+	private Icon chargedIcon;
+	
+	public ForceElectromagnetic(int id) {
+		super(id);
+		setCreativeTab(CreativeTabs.tabMaterials);
+		setMaxStackSize(1);
+		setUnlocalizedName("QUANTAINIUM_INGOT");
+	}
+	
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister register) {
+		itemIcon = register.registerIcon("qmech:ForceElectromagnetic");
+	}
+	
+	
+}
